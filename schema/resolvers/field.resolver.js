124a74c2b3c786deb1f8ac0667a2ee94c286a93d
pboussekey <contact@paul-boussekey.com>
2019-06-05 10:40:21 +0200

    Fields search params

--- schema/resolvers/field.resolver.js
+++ schema/resolvers/field.resolver.js
@@ -1,15 +1,32 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLList, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLString } = graphql;
 const Field = require('../defs/field.def');
 const Db = require('../../database/database');
 
 module.exports = new GraphQLObjectType({
   name: `FieldResolver`,
   fields: {
-    'fields': {
+    fields: {
       type: new GraphQLList(Field),
+      args: {school_id : {type: GraphQLID}, search : {type: GraphQLString}},
       resolve(parent, args, context){
-        return Db.Field.findAll();
+        return Db.sequelize
+        .query(
+          `SELECT
+          field.*
+          FROM
+          field
+          JOIN school_field ON (field.id = school_field.field_id)
+          WHERE field.deleted_at IS NULL
+          ${args.search ? 'AND LCASE(field.name) LIKE :search' : ''}
+          ${args.school_id ? 'AND school_field.school_id = :school' : ''}
+          GROUP BY field.id`,
+          {
+            replacements: { school: args.school_id, search : args.search ? args.search.toLowerCase() + '%' : null },
+            type: Db.Sequelize.QueryTypes.SELECT,
+            model: Db.Field,
+            mapToModel: true
+          });
       }
     }
   }
